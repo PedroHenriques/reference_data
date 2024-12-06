dfd59da0f99ac1fb20ea0abf679ed22b149ef22b
Pedro Henriques <pedro@pedrojhenriques.com>
2024-12-06 17:05:39 +0000

    initial commit.

---
+++ cli/start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e;
+
+docker compose -f setup/local/docker-compose.yml -p myapp build;
+docker compose -f setup/local/docker-compose.yml -p myapp up --no-build;
---
+++ cli/test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e;
+
+WATCH=0;
+PROJ="";
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -w|--watch) WATCH=1; shift 1;;
+
+    -*) echo "unknown option: $1" >&2; exit 1;;
+    *) PROJ=$1; shift 1;;
+  esac
+done
+
+CMD="dotnet test ${PROJ}";
+
+if [ $WATCH -eq 1 ]; then
+  if [ -z "$PROJ" ]; then
+    echo "In watch mode a project name or path must be provided as argument." >&2; exit 1;
+  fi
+
+  CMD="dotnet watch test -q --project ${PROJ}";
+fi
+
+docker run --rm -it -v "./:/app/" -w "/app/" mcr.microsoft.com/dotnet/sdk:8.0-noble /bin/sh -c "$CMD";
