2023d696b9b29a4739254ae3d7470f96305d604e
Pedro Henriques <pedro@pedrojhenriques.com>
2024-12-26 16:22:12 +0000

    Add custom binder to the Entity model to support receiving the Id from the path.

--- src/Api/Models/Entity.cs
+++ src/Api/Models/Entity.cs
@@ -1,20 +1,43 @@
+using System.Reflection;
 using System.Text.Json.Serialization;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 
 namespace Api.Models;
 
 public class Entity
 {
+  [JsonPropertyName("id")]
+  [JsonProperty("id")]
   [BsonId]
   [BsonRepresentation(BsonType.ObjectId)]
   [BsonIgnoreIfDefault]
   public string? Id { get; set; }
 
+  [JsonPropertyName("name")]
+  [JsonProperty("name")]
   [BsonElement("name")]
-  public required string Name { get; set; }
+  public string? Name { get; set; }
 
   [JsonPropertyName("description")]
+  [JsonProperty("description")]
   [BsonElement("description")]
   public string? Desc { get; set; }
+
+  public static async ValueTask<Entity?> BindAsync(HttpContext context)
+  {
+    Entity? entity;
+
+    string bodyAsText = await new StreamReader(context.Request.Body)
+      .ReadToEndAsync();
+
+    entity = JsonConvert.DeserializeObject<Entity>(bodyAsText);
+    if (entity == null)
+    {
+      throw new Exception("Deserializing Entity produced NULL.");
+    }
+
+    return entity;
+  }
 }
