606675405a8db474ea18f689279478c58195a1ec
Pedro Henriques <pedro@pedrojhenriques.com>
2024-12-31 16:10:32 +0000

    Allow passing arguments to the test cli script.

--- cli/start.sh
+++ cli/start.sh
@@ -2,4 +2,4 @@
 set -e;
 
 docker compose -f setup/local/docker-compose.yml -p myapp build;
-docker compose -f setup/local/docker-compose.yml -p myapp up --no-build;
+docker compose -f setup/local/docker-compose.yml -p myapp up --no-build $1;
