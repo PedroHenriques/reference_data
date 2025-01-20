8e88ce672e34f2ac585ae3b451003e86058032eb
Pedro Henriques <pedro@pedrojhenriques.com>
2025-01-20 22:05:25 +0000

    Add notification project initial structure + it's test project.

---
+++ src/Notification/Program.cs
@@ -0,0 +1,35 @@
+﻿using SharedLibs;
+using StackExchange.Redis;
+
+string? redisConStr = Environment.GetEnvironmentVariable("REDIS_CON_STR");
+if (redisConStr == null)
+{
+  throw new Exception("Could not get the 'REDIS_CON_STR' environment variable");
+}
+ConfigurationOptions redisConOpts = new ConfigurationOptions
+{
+  EndPoints = { redisConStr },
+};
+IConnectionMultiplexer? redisClient = ConnectionMultiplexer.Connect(redisConOpts);
+if (redisClient == null)
+{
+  throw new Exception("Redis Client returned NULL.");
+}
+
+redisConStr = Environment.GetEnvironmentVariable("REDIS_CON_STR_QUEUE");
+if (redisConStr == null)
+{
+  throw new Exception("Could not get the 'REDIS_CON_STR_QUEUE' environment variable");
+}
+redisConOpts = new ConfigurationOptions
+{
+  EndPoints = { redisConStr },
+};
+IConnectionMultiplexer? redisClientQueue = ConnectionMultiplexer.Connect(redisConOpts);
+if (redisClient == null)
+{
+  throw new Exception("Redis Client, for the queue, returned NULL.");
+}
+
+ICache cache = new Cache(redisClient);
+IQueue queue = new Cache(redisClientQueue);
