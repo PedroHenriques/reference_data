b53afb4ff4fbf341631b907c8fdd43fb6996c0ca
Pedro Henriques <pedro@pedrojhenriques.com>
2025-03-30 23:34:38 +0000

    Add feature flag handling to the Api service's entrypoint file.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -5,6 +5,11 @@ using System.Diagnostics.CodeAnalysis;
 using Toolkit;
 using Toolkit.Types;
 using MongodbUtils = Toolkit.Utils.Mongodb;
+using FFUtils = Toolkit.Utils.FeatureFlags;
+using FFConfigs = SharedLibs.Configs.FeatureFlags;
+using FFService = Api.Services.FeatureFlags;
+using FFApiConfigs = Api.Configs.FeatureFlags;
+using GeneralConfigs = SharedLibs.Configs.General;
 
 [ExcludeFromCodeCoverage(Justification = "Not unit testable due to instantiating classes for service setup.")]
 internal class Program
@@ -21,6 +26,20 @@ internal class Program
       var inputs = MongodbUtils.PrepareInputs(DbConfigs.MongoConStr);
       return new Mongodb(inputs);
     });
+    builder.Services.AddSingleton<IFeatureFlags>(sp =>
+    {
+      EnvNames ffEnvName;
+      if (FFConfigs.EnvName.TryGetValue(GeneralConfigs.DeploymentEnv, out ffEnvName) == false)
+      {
+        throw new Exception("The value provided in the 'DEPLOYMENT_ENV' environment variable does not map to any valid FeatureFlag environment name.");
+      }
+
+      var inputs = FFUtils.PrepareInputs(
+        FFConfigs.EnvSdkKey, FFConfigs.ContextApiKey, FFConfigs.ContextName,
+        ffEnvName
+      );
+      return new FeatureFlags(inputs);
+    });
     builder.Services.AddScoped<EntityModel>();
 
     WebApplication app = builder.Build();
@@ -30,6 +49,11 @@ internal class Program
       app.UseSwagger();
       app.UseSwaggerUI();
     }
+
+    new FFService(
+      app.Services.GetService<IFeatureFlags>(),
+      [FFApiConfigs.MasterKillSwitch]
+    );
 
     Entities entitiesRouter = new Entities(app);
     EntityData entityDataRouter = new EntityData(app);
