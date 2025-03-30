5739ebc4c872b2c3e04f722e2abc5a5558019a74
Pedro Henriques <pedro@pedrojhenriques.com>
2025-03-30 23:27:16 +0000

    Add functionality, in the Api service, to subscribe to feature flags.

---
+++ src/Api/Services/FeatureFlags.cs
@@ -0,0 +1,30 @@
+using Toolkit.Types;
+
+namespace Api.Services;
+
+public class FeatureFlags
+{
+  public static Dictionary<string, bool> FlagValues;
+  private readonly IFeatureFlags _ff;
+
+  public FeatureFlags(IFeatureFlags ff, string[] flagKeys)
+  {
+    this._ff = ff;
+
+    FlagValues = new Dictionary<string, bool>();
+
+    WatchKeys(flagKeys);
+  }
+
+  private void WatchKeys(string[] flagKeys)
+  {
+    foreach (var key in flagKeys)
+    {
+      FlagValues[key] = this._ff.GetBoolFlagValue(key);
+      this._ff.SubscribeToValueChanges(
+        key,
+        ev => { FlagValues[ev.Key] = ev.NewValue.AsBool; }
+      );
+    }
+  }
+}
---
+++ test/unit/Api.Tests/Services/FeatureFlags.cs
@@ -0,0 +1,169 @@
+using Api.Services;
+using LaunchDarkly.Sdk;
+using LaunchDarkly.Sdk.Server.Interfaces;
+using Moq;
+using Toolkit.Types;
+
+namespace Api.Tests.Services;
+
+[Trait("Type", "Unit")]
+public class FeatureFlagsTests : IDisposable
+{
+  private readonly Mock<IFeatureFlags> _featureFlagsMock;
+
+  public FeatureFlagsTests()
+  {
+    this._featureFlagsMock = new Mock<IFeatureFlags>(MockBehavior.Strict);
+
+    this._featureFlagsMock.Setup(s => s.GetBoolFlagValue(It.IsAny<string>()))
+      .Returns(true);
+    this._featureFlagsMock.Setup(s => s.SubscribeToValueChanges(It.IsAny<string>(), It.IsAny<Action<FlagValueChangeEvent>>()));
+  }
+
+  public void Dispose()
+  {
+    this._featureFlagsMock.Reset();
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_ItShouldCallGetBoolFlagValueFromTheProvidedIFeatureFlagInstanceThreeTimes()
+  {
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    this._featureFlagsMock.Verify(m => m.GetBoolFlagValue(It.IsAny<string>()), Times.Exactly(3));
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_FirstCallToGetBoolFlagValueFromTheProvidedIFeatureFlagInstance_ItShouldHaveTheFirstKey()
+  {
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    this._featureFlagsMock.Verify(m => m.GetBoolFlagValue("test flag key"), Times.Once());
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_SecondCallToGetBoolFlagValueFromTheProvidedIFeatureFlagInstance_ItShouldHaveTheSecondKey()
+  {
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    this._featureFlagsMock.Verify(m => m.GetBoolFlagValue("key 2"), Times.Once());
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_ThirdCallToGetBoolFlagValueFromTheProvidedIFeatureFlagInstance_ItShouldHaveTheThirdKey()
+  {
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    this._featureFlagsMock.Verify(m => m.GetBoolFlagValue("something"), Times.Once());
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_ItShouldSetEachFlagValueInThePublicPropertyFlagValues()
+  {
+    this._featureFlagsMock.SetupSequence(s => s.GetBoolFlagValue(It.IsAny<string>()))
+      .Returns(false).Returns(true).Returns(false);
+
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    Assert.Equal(
+      new Dictionary<string, bool> {
+        { "test flag key", false },
+        { "key 2", true },
+        { "something", false },
+      },
+      FeatureFlags.FlagValues
+    );
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_ItShouldCallSubscribeToValueChangesFromTheProvidedIFeatureFlagInstanceThreeTimes()
+  {
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    this._featureFlagsMock.Verify(m => m.SubscribeToValueChanges(It.IsAny<string>(), It.IsAny<Action<FlagValueChangeEvent>>()), Times.Exactly(3));
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_FirstCallToSubscribeToValueChangesFromTheProvidedIFeatureFlagInstance_ItShouldHaveTheFirstKeyAndAHandler()
+  {
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    this._featureFlagsMock.Verify(m => m.SubscribeToValueChanges("test flag key", It.IsAny<Action<FlagValueChangeEvent>>()), Times.Once());
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_FirstCallToSubscribeToValueChangesFromTheProvidedIFeatureFlagInstance_InvokingTheHandler_ItShouldUpdateTheFlagValueInThePublicPropertyFlagValues()
+  {
+    this._featureFlagsMock.SetupSequence(s => s.GetBoolFlagValue(It.IsAny<string>()))
+      .Returns(true).Returns(false).Returns(true);
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    var testEvent = new FlagValueChangeEvent("test flag key", LdValue.Of(true), LdValue.Of(false));
+
+    (this._featureFlagsMock.Invocations[1].Arguments[1] as Action<FlagValueChangeEvent>)(testEvent);
+    Assert.Equal(
+      new Dictionary<string, bool> {
+        { "test flag key", false },
+        { "key 2", false },
+        { "something", true },
+      },
+      FeatureFlags.FlagValues
+    );
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_SecondCallToSubscribeToValueChangesFromTheProvidedIFeatureFlagInstance_ItShouldHaveTheSecondKeyAndAHandler()
+  {
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    this._featureFlagsMock.Verify(m => m.SubscribeToValueChanges("key 2", It.IsAny<Action<FlagValueChangeEvent>>()), Times.Once());
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_SecondCallToSubscribeToValueChangesFromTheProvidedIFeatureFlagInstance_InvokingTheHandler_ItShouldUpdateTheFlagValueInThePublicPropertyFlagValues()
+  {
+    this._featureFlagsMock.SetupSequence(s => s.GetBoolFlagValue(It.IsAny<string>()))
+      .Returns(true).Returns(false).Returns(true);
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    var testEvent = new FlagValueChangeEvent("key 2", LdValue.Of(false), LdValue.Of(true));
+
+    (this._featureFlagsMock.Invocations[3].Arguments[1] as Action<FlagValueChangeEvent>)(testEvent);
+    Assert.Equal(
+      new Dictionary<string, bool> {
+        { "test flag key", true },
+        { "key 2", true },
+        { "something", true },
+      },
+      FeatureFlags.FlagValues
+    );
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_ThirdCallToSubscribeToValueChangesFromTheProvidedIFeatureFlagInstance_ItShouldHaveTheThirdKeyAndAHandler()
+  {
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    this._featureFlagsMock.Verify(m => m.SubscribeToValueChanges("something", It.IsAny<Action<FlagValueChangeEvent>>()), Times.Once());
+  }
+
+  [Fact]
+  public void Constructor_IfTheKeysArrayHasThreeValues_ThirdCallToSubscribeToValueChangesFromTheProvidedIFeatureFlagInstance_InvokingTheHandler_ItShouldUpdateTheFlagValueInThePublicPropertyFlagValues()
+  {
+    this._featureFlagsMock.SetupSequence(s => s.GetBoolFlagValue(It.IsAny<string>()))
+      .Returns(true).Returns(false).Returns(true);
+    var sut = new FeatureFlags(this._featureFlagsMock.Object, ["test flag key", "key 2", "something"]);
+
+    var testEvent = new FlagValueChangeEvent("something", LdValue.Of(true), LdValue.Of(false));
+
+    (this._featureFlagsMock.Invocations[3].Arguments[1] as Action<FlagValueChangeEvent>)(testEvent);
+    Assert.Equal(
+      new Dictionary<string, bool> {
+        { "test flag key", true },
+        { "key 2", false },
+        { "something", false },
+      },
+      FeatureFlags.FlagValues
+    );
+  }
+}
