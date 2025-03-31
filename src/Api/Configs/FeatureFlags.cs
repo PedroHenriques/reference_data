e36ae24ad03faa6c44c177fbea59e4bf9ce48145
Pedro Henriques <pedro@pedrojhenriques.com>
2025-03-31 10:30:17 +0000

    Change env var name.

--- src/Api/Configs/FeatureFlags.cs
+++ src/Api/Configs/FeatureFlags.cs
@@ -5,6 +5,6 @@ namespace Api.Configs;
 [ExcludeFromCodeCoverage(Justification = "Not unit testable due to being a config static class.")]
 public static class FeatureFlags
 {
-  public static string MasterKillSwitch = Environment.GetEnvironmentVariable("LD_KILL_SWITCH_KEY")
-    ?? throw new Exception("Could not get the 'LD_KILL_SWITCH_KEY' environment variable");
+  public static string ApiKeyActive = Environment.GetEnvironmentVariable("LD_API_ACTIVE_KEY")
+    ?? throw new Exception("Could not get the 'LD_API_ACTIVE_KEY' environment variable");
 }
